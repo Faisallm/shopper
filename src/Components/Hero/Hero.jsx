7cd0b4980e41596e5b807a12d06ea18cfc99d159
Faisallm <faisallawan1997@gmail.com>
2023-10-09 23:31:18 -0700

    Hero Section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./hero.css";
+import handIcon from '../Assets/hand_icon.png';
+import arrowIcon from '../Assets/arrow.png';
+import HeroImage from '../Assets/hero_image.png';
+
+const Hero = () => {
+  return <div className="hero">
+    {/* left column */}
+    <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+            <div className="hand-hand-icon">
+                <p>New</p>
+                <img src={handIcon} alt="Hand Icon" />
+            </div>
+            <p>collections</p>
+            <p>for everyone</p>
+        </div>
+        <div className="hero-latest-btn">
+            <div>Latest Collection</div>
+            <img src={arrowIcon} alt="Arrow Icon" />
+        </div>
+    </div>
+    {/* right column */}
+    <div className="hero-right">
+        <img src={HeroImage} alt="Hero-image" />
+    </div>
+  </div>;
+};
+
+export default Hero;
